b2734857ebe83bebaa6838f047b1aa7f3b312d28
Dikeledi Mokoatle <2769106@students.wits.ac.za>
2026-04-04 23:08:08 +0200

    feat: add basic User schema with firstname, lastname, email, password, roles

---
+++ backend/common/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+
+const userSchema = new Schema({
+    
+    firstName: {type:String, required:true, trim:true},
+    lastName: {type:String, required:true, trim:true},
+    email: {type:String, required:true, unique:true, lowercase:true, trim:true},
+    password: {type: String, required:true},
+    roles: {type: [String], default: ['Applicant']},
+    craetedAt: {type:Date, default: Date.now}
+});
+
+const User = model('User', userSchema);
+module.exports = User;
