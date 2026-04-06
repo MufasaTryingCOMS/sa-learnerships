7f58fb86e342ffa82d0fb8dd832c0d93e1f69611
Dikeledi Mokoatle <2769106@students.wits.ac.za>
2026-04-06 16:36:59 +0200

    feat(auth): implement front end google registration logic

--- register/register.js
+++ register/register.js
@@ -5,10 +5,37 @@ const lastName = document.getElementById('lastName');
 const email = document.getElementById('email');
 const password = document.getElementById('newPassword');
 const confirmPassword = document.getElementById('confirmPassword');
+const googleBtn = document.getElementById('register-google');
 
+google.accounts.id.initialize({
+    client_id: "540897393879-so4uoeddh7jvu1delkd6ear8t7t9v0dt.apps.googleusercontent.com",
+    callback: receiveGoogleToken
+});
 
+function isStrong(password){
+    let hasLowercase = false;
+    let hasUppercase = false;
+    let hasDigit = false;
+    let SpecialSymbols = ['!','@','#','$','%','&','*'];
+    let HasSpecialSymbols = false;
 
-import {isStrong} from '../scripts/common_functions.js';
+    for (let x of password){
+        if (x >= 'A' && x <= 'Z'){
+            hasUppercase = true;
+        }
+        else if(x >= 'a' && x <= 'z'){
+            hasLowercase = true;
+        }
+        else if (x >='0' && x <= '9'){
+            hasDigit = true;
+        }
+        else if (SpecialSymbols.includes(x)){
+            HasSpecialSymbols = true;
+        }
+    }
+
+    return hasLowercase && hasUppercase && hasDigit && HasSpecialSymbols;   
+}
 
 form.addEventListener('submit', async(event)=>{
     event.preventDefault();
@@ -50,9 +77,9 @@ form.addEventListener('submit', async(event)=>{
 
         const data = await response.json();
         if (data.success){
-            const successMesaage = document.getElementById('success-message');
-            successMesaage.textContent = "Registered successfully";
-            successMesaage.style.display = "block";
+            const successMessage = document.getElementById('success-message');
+            successMessage.textContent = "Registered successfully";
+            successMessage.style.display = "block";
             
             setTimeout(()=>{
                 window.location.href = ""; //I will handle navigation once the required page has been created:)
@@ -72,3 +99,56 @@ form.addEventListener('submit', async(event)=>{
 
     
 })
+
+
+async function receiveGoogleToken(res){
+    const google_token = res.credential;
+
+    try{
+        const response = await fetch('http://localhost:3000/registerGoogle', {
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify({token : google_token})
+        });
+
+        const data = await response.json();
+        errorMessage.style.display = "none";
+       
+
+        if (data.success){
+
+            const successMessage = document.getElementById('success-message');
+            successMessage.textContent = "Registered successfully with Google";
+            successMessage.style.display = "block";
+            
+            setTimeout(()=>{
+                window.location.href = ""; //I will handle navigation once the required page has been created:)
+            }, 3000);
+            
+        }
+        else{
+            errorMessage.textContent = data.error || "Google register failed!";
+            errorMessage.style.display = "block";
+        }
+
+    }catch(err){
+        errorMessage.style.display = "block";
+        errorMessage.textContent = err.message;
+    }
+
+
+
+
+}
+
+google.accounts.id.renderButton(
+    document.getElementById("google-btn-container"),
+    { theme: "outline", size: "large" }
+);
+
+googleBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const googleButton = document.querySelector("#google-btn-container div[role='button']");
+    if (googleButton) googleButton.click();
+});
