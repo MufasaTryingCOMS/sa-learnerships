931078e3beead99ceb0e458758fda5de18b14a6c
Dikeledi Mokoatle <2769106@students.wits.ac.za>
2026-04-06 16:26:35 +0200

    feat: add Google register route

--- backend/authorization/routes.js
+++ backend/authorization/routes.js
@@ -2,5 +2,6 @@ const express = require('express');
 const controller = require('./controller');
 const router = express.Router();
 router.post('/register', controller.register);
+router.post('/registerGoogle', controller.registerGoogle);
 
 module.exports = router;
