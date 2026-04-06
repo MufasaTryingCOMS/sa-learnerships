dd346a8713a1cdd14f66a9a8a4e0445ecdb5d95e
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-04-06 14:05:50 +0200

    Integrate api opportunity creation with the frontend

---
+++ opportunities/scripts/create.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('create-opportunity-form');
+const title = document.getElementById('title');
+const closingDate = document.getElementById('closing-date');
+const errorMessage = document.getElementById('error-message');
+const submitBtn = document.getElementById('submit-btn');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    if (!title.value) {
+        errorMessage.style.display = 'block';
+        errorMessage.innerHTML = 'Title required! Please provide the title of the opportunity';
+        return;
+    }
+
+    if (!closingDate.value) {
+        errorMessage.style.display = 'block';
+        errorMessage.innerHTML = 'Closing date required! Please provide the closing date of the opportunity';
+        return;
+    }
+
+    submitBtn.disabled = true;
+    submitBtn.textContent = 'Loading...';
+
+    try {
+        const response = await fetch('http://localhost:3000/opportunities', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                title: title.value,
+                closingDate: closingDate.value,
+            }),
+        });
+
+        if (response.status === 201) {
+            // TODO: Navigate the user to a successful opportunity creation page
+            console.log(await response.json());
+        }
+    } catch (error) {
+        errorMessage.style.display = 'block';
+        errorMessage.textContent = 'Server error';
+        console.error('Login error:', err);
+    } finally {
+        submitBtn.disabled = false;
+        submitBtn.textContent = 'Submit for review';
+    }
+});
