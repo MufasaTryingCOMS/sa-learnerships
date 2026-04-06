b60269d18b56a7bc0e8eb6ebe5d22c767528eb79
Dikeledi Mokoatle <2769106@students.wits.ac.za>
2026-04-06 16:33:02 +0200

    feat(db): add Google ID and registration method to User Schema

--- backend/common/models/User.js
+++ backend/common/models/User.js
@@ -8,8 +8,10 @@ const userSchema = new Schema({
     firstName: {type:String, required:true, trim:true},
     lastName: {type:String, required:true, trim:true},
     email: {type:String, required:true, unique:true, lowercase:true, trim:true},
-    password: {type: String, required:true},
+    password: {type: String},
     roles: {type: [String], default: ['Applicant']},
+    googleId : {type: String},
+    signupMethod: {type: String},
     createdAt: {type:Date, default: Date.now}
 });
 
