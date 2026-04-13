0a21105c0378f161b17eed3d2de07ca775ef2749
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-04-13 11:02:27 +0200

    Add a dummy test

--- backend/__tests__/dummy.test.js
+++ backend/__tests__/dummy.test.js
@@ -1,6 +1,6 @@
 // Dummy test case that always pass so that we don't get the 'no tests found' error from jest
 describe('Dummy', () => {
     test('should always pass', () => {
-      	expect(true).toBe(true);
+        expect(true).toBe(true);
     });
 });
